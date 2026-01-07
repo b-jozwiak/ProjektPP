16c6a18790a5b20f57ee48b613bb02cadc154826
b-jozwiak <225209357+b-jozwiak@users.noreply.github.com>
2026-01-07 08:40:41 +0100

    Informacje o bohaterze
    Dodano nagłówek hero.h zawierający minimalne informacje o bohaterze wymienione w temacie projektu

---
+++ src/hero/hero.h
@@ -0,0 +1,41 @@
+#ifndef HERO_H
+#define HERO_H
+
+// Ilość znaków w imieniu bohatera. Terminator null nie jest wliczany.
+#define MAX_HERO_NAME_LENGTH 50
+
+typedef enum {
+    WARRIOR,
+    MAGE,
+    PRIEST,
+    ROGUE,
+    HUNTER,
+    DRUID
+} HeroClass;
+
+typedef enum {
+    HUMAN,
+    ELF,
+    DWARF,
+    ORC,
+    TIEFLING
+} HeroRace;
+
+typedef enum {
+    ACTIVE,
+    ON_QUEST,
+    INJURED,
+    MISSING,
+    SUSPENDED
+} HeroStatus;
+
+typedef struct {
+    char name[MAX_HERO_NAME_LENGTH + 1];
+    HeroRace race;
+    HeroClass hero_class;
+    int experience_level;
+    int reputation;
+    HeroStatus status;
+} Hero;
+
+#endif // HERO_H
