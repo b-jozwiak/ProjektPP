f3180728407a72624cb0bdfcc24ca2c75384eee7
b-jozwiak <225209357+b-jozwiak@users.noreply.github.com>
2026-01-10 21:07:03 +0100

    Przyjmowanie argumentów
    Przyjmowanie argumentów przez program. Jeszcze nie są przekazywane jako nazwy plików.

--- src/main.c
+++ src/main.c
@@ -7,7 +7,22 @@
 #include <string.h>
 
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc < 2 || argc > 3) {
+        if (argc > 3) {
+            fprintf(stderr, "Zbyt duzo argumentow.\n\n");
+        }
+        fprintf(stderr, "Uzycie: %s <plik_danych> [plik_zapisu]\n", argv[0]);
+        fprintf(stderr, "Przyklad 1: %s bohaterowie.txt\n", argv[0]);
+        fprintf(stderr, "Przyklad 2: %s wczytaj.txt zapisz.txt\n\n", argv[0]);
+        fprintf(stderr, "Jezeli zostanie podany tylko jeden plik, bedzie on sluzyl jako plik wczytu i zapisu\n");
+        return 1;
+    }
+
+    const char* data_file = argv[1];
+    const char* output_file = argc > 2 ? argv[2] : data_file;
+
+
     HeroList* hero_list = init_hero_list();
     if (hero_list == NULL) {
         fprintf(stderr, "Blad tworzenia listy bohaterow.\n");
