0908ef9b922e32fd39ef8ae731bb2b2b5039f531
b-jozwiak <225209357+b-jozwiak@users.noreply.github.com>
2026-01-10 21:47:42 +0100

    Dokumentacja file_handler.h
    Dodanie dokumentacji dla nagłówka w jednolitym stylu do reszty programu.

--- src/io/file_handler.h
+++ src/io/file_handler.h
@@ -5,7 +5,34 @@
 
 #define MAX_FILE_NAME_LENGTH 256
 
+/**
+ * Funkcja zapisująca listę bohaterów do pliku tekstowego.
+ *
+ * HeroList* list - Wskaźnik do listy bohaterów do zapisania.
+ * const char* file_path - Ścieżka do docelowego pliku.
+ * 
+ * Format pliku:
+ *   HERO_LIST N
+ *   name;race;class;level;reputation;status
+ *   ...
+ * 
+ * Funkcja zwraca true jeśli zapis powiódł się, false w przypadku błędu.
+ */
 bool save_list_to_file(HeroList* list, const char* file_path);
+
+/**
+ * Funkcja wczytująca listę bohaterów z pliku tekstowego.
+ *
+ * const char* file_path - Ścieżka do pliku źródłowego.
+ * 
+ * Waliduje:
+ *   - Format nagłówka (HERO_LIST N)
+ *   - Poprawność enumeracji (race, class, status)
+ *   - Zakresy wartości (level 1-100, reputation 0-100)
+ *   - Kompletność danych (6 pól na bohatera)
+ * 
+ * Funkcja zwraca wskaźnik do nowej listy bohaterów, lub NULL jeśli błąd.
+ */
 HeroList* load_list_from_file(const char* file_path);
 
 #endif // FILE_HANDLER_H
