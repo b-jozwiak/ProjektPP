556c355b93c627835c32f1de764d7d1499d02b45
b-jozwiak <225209357+b-jozwiak@users.noreply.github.com>
2026-01-10 21:48:19 +0100

    Dokumentacja main_menu.h i hero_menu.h
    Dodanie dokumentacji w jednolitym stylu do reszty programu

--- src/ui/hero_menu.h
+++ src/ui/hero_menu.h
@@ -3,6 +3,23 @@
 
 #include "../hero/hero_list.h"
 
+/**
+ * Funkcja wyświetlająca menu zarządzania bohaterami.
+ *
+ * HeroList* hero_list - Wskaźnik do głównej listy bohaterów.
+ * const char* output_file - Domyślna ścieżka do pliku zapisu bohaterów.
+ * 
+ * Menu umożliwia:
+ *   - Wyświetlanie listy bohaterów
+ *   - Czyszczenie filtrów
+ *   - Wyszukiwanie i filtrowanie (9 kryteriów)
+ *   - Sortowanie (6 sposobów)
+ *   - Edycję danych bohaterów
+ *   - Usuwanie bohaterów
+ *   - Zapis wybranej listy do pliku
+ * 
+ * Funkcja nic nie zwraca. Wraca do menu głównego po wyborze opcji 0.
+ */
 void hero_menu(HeroList* hero_list, const char* output_file);
 
 #endif // HERO_MENU_H
--- src/ui/main_menu.h
+++ src/ui/main_menu.h
@@ -3,7 +3,23 @@
 
 #include "../hero/hero_list.h"
 
+/**
+ * Funkcja wyświetlająca główne menu aplikacji.
+ *
+ * HeroList** hero_list - Wskaźnik do wskaźnika listy bohaterów 
+ *                         (może być zmieniana przy wczytywaniu z pliku).
+ * const char* input_file - Domyślna ścieżka do pliku wczytania bohaterów.
+ * const char* output_file - Domyślna ścieżka do pliku zapisu bohaterów.
+ * 
+ * Menu umożliwia:
+ *   1. Rejestrację nowego bohatera
+ *   2. Dostęp do menu zarządzania bohaterami
+ *   3. Wczytanie bohaterów z pliku
+ *   4. Zapis bohaterów do pliku
+ *   0. Wyjście z programu
+ * 
+ * Funkcja nic nie zwraca. Pętla się, aż użytkownik wybierze opcję 0.
+ */
 void main_menu(HeroList** hero_list, const char* input_file, const char* output_file);
-
 
 #endif // MAIN_MENU_H
